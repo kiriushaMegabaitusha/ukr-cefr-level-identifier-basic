5f24e5bdeb361bd06a5a037eba34f76bd9bf9108
kiril <kasuda@kse.org.ua>
2024-05-22 23:47:52 +0300

    added readability_utils.py, text_index.ipynb modified cefr_check_utils.py

--- cefr_check_utils.py
+++ cefr_check_utils.py
@@ -1,7 +1,18 @@
 
+grammemes_dict = {
+    'A1': {},
+    'A2': {},
+    'B1': {},
+    'B2': {},
+    'C1': {},
+    'C2': {}
+}
 def check_grammemes_level(grammemes):
     '''
     Calculate CEFR level based on grammar index
-    :param grammemes: string containing morhpological categories
-    :return: CEFR level based on the grammeme index
+    :param grammemes: string containing morhpological categories in pymorphy2 format
+    :return: CEFR level based on the grammeme dictionary
     '''
+    if grammemes not in grammemes_dict:
+        return None
+    elif grammemes in grammemes_dict['A1']:
