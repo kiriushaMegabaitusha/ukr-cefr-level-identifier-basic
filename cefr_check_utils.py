3afca4e38c4673fae484fe79944365cd0afb9fbf
kiril <kasuda@kse.org.ua>
2024-05-29 23:07:42 +0300

    add and fill grammemes_dict and lemmas_dict

--- cefr_check_utils.py
+++ cefr_check_utils.py
@@ -1,13 +1,106 @@
+# dictionary of grammemes
+
+'''
+A1	['NOUN', 'sing'],
+	['NOUN', 'plur', 'nomn'],
+	['NOUN', 'accs', 'sing'], ['NOUN', 'loct', 'sing'],
+
+
+
+
+
+	['NOUN', 'voct'],
+	['ADJF', 'sing'], ['ADJF', 'plur'],
+	['ADJF', 'nomn'], ['ADJF', 'accs'], ['ADJF', 'loct']
+	['NPRO', 'pers'], ['NPRO']
+
+	['VERB', 'plur']
+	['VERB', 'perf', 'futr']
+	['VERB', 'impr']
+A2	 ['NOUN']
+	['ADJF', 'sing'], ['ADJF, 'plur']
+	['NPRO', 'pers']
+	['NPRO']
+	['VERB', 'pres']
+	боротися' ['VERB', 'pres']
+	['VERB', 'past']
+	['VERB', 'futr']
+	['ADJF', 'COMP']
+	['ADJF', 'Supr']
+	більш', 'менш', 'більше', 'менше'
+	найбільш', 'найменш', 'найбільше', 'найменше'
+	['ADJF', 'COMP'], [ADJF, 'Supr']
+B1	['NOUN']
+	['ADJF']
+	один', 'два', 'три', 'чотири', ['NUMR']
+	багато', 'кілька', 'декілька'
+	['NPRO']
+	себе', 'соб'
+	той', 'цей'
+	['VERB', 'Refl', 'pres']
+	дати', 'їсти' ['VERB', 'pres']
+	['VERB', 'past', 'impf'], ['VERB', 'past', 'perf']
+	['VERB', 'futr', 'impf']
+	['VERB', 'futr', 'perf']
+	['VERB' 'impr'] 'хай', 'нехай', 'нехаяти'
+	би', 'б'
+	['ADJF' 'COMP']
+	['ADJF' 'Supr']
+	більш', 'менш', 'більше', 'менше'
+	найбільш', 'найменш', 'найбільше', 'найменше'
+	['ADVB' 'COMP']
+	['ADVB' 'Supr']
+	більш', 'менш', 'більше', 'менше'
+	найбільш', 'найменш', 'найбільше', 'найменше'
+B2	['NOUN']
+	['NOUN' 'Abbr']
+	['ADJF']
+	їхній'
+	себе', 'соб'
+	['VERB' 'impf', 'pres']
+	['VERB' 'impf', 'futr']
+	дати', 'їсти', ['VERB', 'pres'], ['VERB', 'futr']
+	['VERB' 'perf']
+	['VERB' 'impr'] 'хай', 'нехай', 'нехаяти'
+	би', 'б'
+	['ADJF' 'COMP']
+	['ADJF' 'Supr']
+	більш', 'менш', 'більше', 'менше'
+	найбільш', 'найменш', 'найбільше', 'найменше'
+	['ADVB' 'COMP']
+	['ADVB' 'Supr']
+	'більш', 'менш', 'більше', 'менше', 'найбільш', 'найменш', 'найбільше', 'найменше', 'якщо', 'хоч', 'якби'
+C1	['NOUN']
+	['NOUN' 'Abbr']
+	['ADJF']
+	себе', 'соб'
+	['ADJF' 'COMP'] ['ADJF' 'Supr']
+	більш', 'менш', 'більше', 'менше', 'найбільш', 'найменш', 'найбільше', 'найменше'
+C2	['NOUN']
+	['ADJF']
+	['NPRO']
+	cебе', 'соб'
+'''
 
 grammemes_dict = {
-    'A1': {},
-    'A2': {},
-    'B1': {},
-    'B2': {},
-    'C1': {},
-    'C2': {}
+    'A1': [['NOUN', 'sing'], ['NOUN', 'plur', 'nomn'], ['NOUN', 'accs', 'sing'], ['NOUN', 'loct', 'sing'], ['NOUN', 'voct'], ['ADJF', 'sing'], ['ADJF', 'plur'], ['ADJF', 'nomn'], ['ADJF', 'accs'], ['ADJF', 'loct'], ['NPRO', 'pers'], ['NPRO'], ['VERB', 'plur'], ['VERB', 'perf', 'futr'], ['VERB', 'impr']],
+    'A2': [['NOUN'], ['ADJF', 'sing'], ['ADJF', 'plur'], ['NPRO', 'pers'], ['NPRO'], ['VERB', 'pres'], ['VERB', 'pres'], ['VERB', 'past'], ['VERB', 'futr'], ['ADJF', 'COMP'], ['ADJF', 'Supr'], ['ADJF', 'COMP'], ['ADJF', 'Supr']],
+    'B1': [['NOUN'], ['ADJF'], ['NUMR'], ['NPRO'], ['VERB', 'Refl', 'pres'], ['VERB', 'pres'], ['VERB', 'past', 'impf'], ['VERB', 'past', 'perf'], ['VERB', 'futr', 'impf'], ['VERB', 'futr', 'perf'], ['VERB' 'impr'], ['ADJF' 'COMP'], ['ADJF' 'Supr'], ['ADVB' 'COMP'], ['ADVB' 'Supr']],
+    'B2': [['NOUN'], ['NOUN' 'Abbr'], ['ADJF'], ['VERB' 'impf', 'pres'], ['VERB' 'impf', 'futr'], ['VERB', 'pres'], ['VERB', 'futr'], ['VERB' 'perf'], ['VERB' 'impr'], ['ADJF' 'COMP'], ['ADJF' 'Supr'], ['ADVB' 'COMP'], ['ADVB' 'Supr']],
+    'C1': [['NOUN'], ['NOUN' 'Abbr'], ['ADJF'], ['ADJF' 'COMP'], ['ADJF' 'Supr']],
+    'C2': [['NOUN'], ['ADJF'], ['NPRO']]
 }
-def check_grammemes_level(grammemes):
+
+lemmas_dict = {
+    'A2': ['боротися', 'більш', 'менш', 'більше', 'менше', 'найбільш', 'найменш', 'найбільше', 'найменше'],
+    'B1': ['себе', 'соб', 'той', 'цей', 'один', 'два', 'три', 'чотири',	'багато', 'кілька', 'декілька', 'більш', 'менш', 'більше', 'менше', 'найбільш', 'найменш', 'найбільше', 'найменше'],
+    'B2': ['їхній', 'себе', 'соб', 'дати', 'їсти', 'хай', 'нехай', 'нехаяти', 'би', 'б', 'більш', 'менш', 'більше', 'менше', 'найбільш', 'найменш', 'найбільше', 'найменше', 'якщо', 'хоч', 'якби'],
+    'C1': ['себе', 'соб', 'більш', 'менш', 'більше', 'менше', 'найбільш', 'найменш', 'найбільше', 'найменше'],
+    'C2': ['cебе', 'соб']
+}
+
+#define the level of the grammemes
+de[check_grammemes_level(grammemes):
     '''
     Get highest available CEFR level based on grammar indices
     :param grammemes: string containing morhpological categories in pymorphy2 format
@@ -25,6 +118,8 @@ def check_grammemes_level(grammemes):
         return 5
     elif grammemes in grammemes_dict['A1']:
         return 6
+    else:
+        return 0
 
 def interpret_cefr_list(level_list):
     '''
--- main.py
+++ main.py
@@ -1,3 +1,34 @@
-import data_utils, readability_utils, cefr_check_utils
-import pandas as pd
+import cefr_check_utils, data_utils, readability_utils
 
+from PyQt6.QtWidgets import QApplication, QWidget, QMainWindow, QLabel, QPushButton, QVBoxLayout
+import sys
+
+app = QApplication(sys.argv)
+
+class window(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle('Аналізатор рівня УМІ')
+
+        self.open_button = QPushButton(self)
+        self.open_button.setText('Відкрити txt файл')
+
+        self.open_label = QLabel(self)
+        self.open_label.setText('Для початку роботи відкрийте файл із текстом, що аналізуватиметься.')
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.open_label)
+        layout.addWidget(self.open_button)
+
+
+        container = QWidget(self)
+        container.setLayout(layout)
+
+        self.setCentralWidget(container)
+
+window = window()
+
+window.show()
+
+app.exec()
